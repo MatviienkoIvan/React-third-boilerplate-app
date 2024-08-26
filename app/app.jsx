0a0b1e7528631e3ea21ec0396601b2929126c896
Matviienko.Ivan <matviienko.i.o@gmail.com>
2024-08-26 12:40:47 +0200

    Initial app

---
+++ app/app.jsx
@@ -0,0 +1,15 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+var { Router, Route, IndexRoute, hasHistory } = require("react-router");
+
+// load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
+// App css
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+ <p>Boilerplate 3 Project</p>,
+  document.getElementById("rootApp")
+);
